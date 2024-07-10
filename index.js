7fbd7e0344ea0b854ae5ac3b7e58c3a24cd6ba8c
GHOSTFACE <erickkatana4787@gmail.com>
2024-07-10 08:01:52 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+    }
+    
+  // Call the function to activate the event listener
+  addingEventListener();
+  const newNode = document.createElement('div');
+newNode.textContent = 'ERICK KATANA is the champion';
+document.body.append(newNode);
